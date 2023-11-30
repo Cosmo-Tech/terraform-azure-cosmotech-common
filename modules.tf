a9df79f055d34fbbff6fc65d31284c824e19d39b
nibaldo <nibaldo.donoso@cosmotech.com>
2023-11-30 14:12:51 +0100

    add create_vnet var in common module

--- modules.tf
+++ modules.tf
@@ -24,6 +24,7 @@ module "cosmotech-prerequisites" {
   platform_client_id     = var.platform_client_id
   platform_client_secret = var.platform_client_secret
   platform_subnet_id     = var.platform_subnet_id
+  create_vnet            = var.create_vnet
 }
 
 module "cosmotech-platform" {

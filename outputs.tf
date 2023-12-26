b34966231e6221cdf244d86015ee962eabc079c7
enocquet <emmanuelnocquet8@gmail.com>
2023-12-26 17:49:08 +0100

    fixing outputs to match corrects inputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "out_cluster_name" {
-  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_aks_cluster_name : null
+  value = module.cosmotech-prerequisites.out_aks_cluster_name
 }
 
 output "out_vnet_name" {
-  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_platform_vnet_name : null
+  value = module.cosmotech-prerequisites.out_platform_vnet_name
 }
 
 output "out_platform_sp_name" {
-  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_platform_sp_name : null
+  value = module.cosmotech-prerequisites.out_platform_sp_name
 }
 
 output "out_platform_public_ip" {
-  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_public_ip_name : null
+  value = module.cosmotech-prerequisites.out_public_ip_name
 }

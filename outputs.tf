23de60e836b4ea40ca1d77ae4609f5fe0fc21138
nibaldo <nibaldo.donoso@cosmotech.com>
2024-03-27 11:58:43 +0100

    add outputs for add common

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,15 @@ output "out_public_ip_name" {
 output "out_platform_sp_name" {
   value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_platform_sp_name : null
 }
+
+output "out_network_sp_client_id" {
+  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_networkadt_clientid : null
+}
+
+output "out_network_sp_object_id" {
+  value = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_networkadt_sp_objectid : null
+}
+
+output "out_platform_object_id" {
+  value =  var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_platform_sp_object_id : null
+}

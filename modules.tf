b803d9d2a1a1a07b843c0c0c2df94d88e723f6c3
nibaldo <nibaldo.donoso@cosmotech.com>
2024-04-17 11:50:20 +0200

    add missing ref variables loki

--- modules.tf
+++ modules.tf
@@ -69,23 +69,29 @@ module "cosmotech-prerequisites" {
 module "cosmotech-platform" {
   source = "./platform-common-resources"
 
-  client_id                = var.client_id     # Should be discarded
-  client_secret            = var.client_secret # Should be discarded
-  subscription_id          = var.subscription_id
-  tenant_id                = var.tenant_id
-  cluster_issuer_email     = var.cluster_issuer_email
-  cluster_issuer_name      = var.cluster_issuer_name
-  tls_secret_name          = local.tls_secret_name
-  tls_certificate_type     = var.tls_certificate_type
-  namespace                = var.namespace
-  monitoring_namespace     = var.monitoring_namespace
-  ingress_nginx_version    = var.ingress_nginx_version
-  create_prometheus_stack  = var.create_prometheus_stack
-  publicip_resource_group  = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_ip_resource_group : var.publicip_resource_group
-  api_dns_name             = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_fqdn : var.fqdn
-  resource_group           = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_ip_resource_group : var.resource_group
-  loadbalancer_ip          = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_public_ip : var.loadbalancer_ip
-  kube_config              = module.cosmotech-prerequisites.out_aks_phoenix_config
-  certificate_cert_content = var.tls_certificate_custom_certificate
-  certificate_key_content  = var.tls_certificate_custom_key
+  client_id                        = var.client_id     # Should be discarded
+  client_secret                    = var.client_secret # Should be discarded
+  subscription_id                  = var.subscription_id
+  tenant_id                        = var.tenant_id
+  cluster_issuer_email             = var.cluster_issuer_email
+  cluster_issuer_name              = var.cluster_issuer_name
+  tls_secret_name                  = local.tls_secret_name
+  tls_certificate_type             = var.tls_certificate_type
+  namespace                        = var.namespace
+  monitoring_namespace             = var.monitoring_namespace
+  ingress_nginx_version            = var.ingress_nginx_version
+  create_prometheus_stack          = var.create_prometheus_stack
+  publicip_resource_group          = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_ip_resource_group : var.publicip_resource_group
+  api_dns_name                     = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_fqdn : var.fqdn
+  resource_group                   = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_ip_resource_group : var.resource_group
+  loadbalancer_ip                  = var.deployment_type != "ARM" ? module.cosmotech-prerequisites.out_public_ip : var.loadbalancer_ip
+  kube_config                      = module.cosmotech-prerequisites.out_aks_phoenix_config
+  certificate_cert_content         = var.tls_certificate_custom_certificate
+  certificate_key_content          = var.tls_certificate_custom_key
+  loki_release_name                = var.loki_release_name
+  loki_persistence_memory          = var.loki_persistence_memory
+  loki_retention_period            = var.loki_retention_period
+  helm_repo_url                    = var.helm_repo_url
+  helm_chart                       = var.helm_chart
+  loki_max_entries_limet_per_query = var.loki_max_entries_limet_per_query
 }

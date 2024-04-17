f5a2c4962da4c211c8b1024acec9e8c58808c60c
nibaldo <nibaldo.donoso@cosmotech.com>
2024-04-17 11:50:20 +0200

    add missing ref variables loki

--- variables.tf
+++ variables.tf
@@ -487,3 +487,32 @@ terraform init \
     -backend-config "access_key=$TF_VAR_tf_access_key"
 EOT
 }
+
+variable "loki_release_name" {
+  type    = string
+  default = "loki"
+}
+variable "loki_persistence_memory" {
+  type    = string
+  default = "4Gi"
+}
+
+variable "loki_retention_period" {
+  type    = string
+  default = "720h"
+}
+
+variable "helm_repo_url" {
+  type    = string
+  default = "https://grafana.github.io/helm-charts"
+}
+
+variable "helm_chart" {
+  type    = string
+  default = "loki-stack"
+}
+
+variable "loki_max_entries_limet_per_query" {
+  type    = number
+  default = 50000
+}

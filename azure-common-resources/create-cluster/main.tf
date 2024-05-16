61142dd6235cd177698a46c56503fa1728b774a1
nibaldo <nibaldo.donoso@cosmotech.com>
2024-05-16 17:32:30 +0200

    add variables for aks

--- azure-common-resources/create-cluster/main.tf
+++ azure-common-resources/create-cluster/main.tf
@@ -40,11 +40,11 @@ resource "azurerm_kubernetes_cluster" "phoenixcluster" {
     temporary_name_for_rotation = var.temporary_name_for_rotation_system_pool
     orchestrator_version        = var.kubernetes_version
     vm_size                     = var.kubernetes_nodepool_system_type
-    max_pods                    = 110
-    max_count                   = 6
-    min_count                   = 3
-    enable_auto_scaling         = true
-    os_disk_size_gb             = 128
+    max_pods                    = var.kubernetes_max_system_pods
+    max_count                   = var.kubernetes_max_system_instances
+    min_count                   = var.kubernetes_min_system_instances
+    enable_auto_scaling         = var.kubernetes_system_enable_auto_scaling
+    os_disk_size_gb             = var.kubernetes_system_os_disk_size
     os_disk_type                = "Managed"
     vnet_subnet_id              = var.subnet_id
   }
@@ -61,13 +61,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "basic" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_basic_compute_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_basic_pods
   max_count             = var.kubernetes_max_basic_compute_instances
-  min_count             = 1
-  enable_auto_scaling   = true
+  min_count             = var.kubernetes_min_basic_compute_instances
+  enable_auto_scaling   = var.kubernetes_basic_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_basic_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "compute", "cosmotech.com/size" = "basic" }
@@ -86,13 +86,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "highcpu" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_highcpu_compute_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_highcpu_pods
   max_count             = var.kubernetes_max_highcpu_compute_instances
-  min_count             = 0
-  enable_auto_scaling   = true
+  min_count             = var.kubernetes_min_highcpu_compute_instances
+  enable_auto_scaling   = var.kubernetes_highcpu_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_highcpu_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "compute", "cosmotech.com/size" = "highcpu" }
@@ -111,13 +111,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "highmemory" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_highmemory_compute_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_highmemory_pods
   max_count             = var.kubernetes_max_highmemory_compute_instances
-  min_count             = 0
-  enable_auto_scaling   = true
+  min_count             = var.kubernetes_min_highmemory_compute_instances
+  enable_auto_scaling   = var.kubernetes_highmemory_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_highmemory_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "compute", "cosmotech.com/size" = "highmemory" }
@@ -136,13 +136,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "services" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_services_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_services_pods
   max_count             = var.kubernetes_max_services_instances
-  min_count             = 2
-  enable_auto_scaling   = true
+  min_count             = var.kubernetes_min_services_instances
+  enable_auto_scaling   = var.kubernetes_services_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_services_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "services" }
@@ -161,13 +161,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "db" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_db_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_db_pods
   max_count             = var.kubernetes_max_db_instances
   min_count             = var.kubernetes_min_db_instances
-  enable_auto_scaling   = true
+  enable_auto_scaling   = var.kubernetes_db_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_db_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "db" }
@@ -186,13 +186,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "monitoring" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.phoenixcluster.id
   orchestrator_version  = var.kubernetes_version
   vm_size               = var.kubernetes_monitoring_type
-  max_pods              = 110
+  max_pods              = var.kubernetes_max_monitoring_pods
   max_count             = var.kubernetes_max_monitoring_instances
-  min_count             = 1
-  enable_auto_scaling   = true
+  min_count             = var.kubernetes_min_monitoring_instances
+  enable_auto_scaling   = var.kubernetes_monitoring_enable_auto_scaling
   mode                  = "User"
   os_type               = "Linux"
-  os_disk_size_gb       = 128
+  os_disk_size_gb       = var.kubernetes_monitoring_os_disk_size
   os_disk_type          = "Managed"
   node_taints           = ["vendor=cosmotech:NoSchedule"]
   node_labels           = { "cosmotech.com/tier" = "monitoring" }
--- azure-common-resources/modules.tf
+++ azure-common-resources/modules.tf
@@ -17,6 +17,7 @@ module "create-cluster" {
   kubernetes_min_db_instances                 = var.kubernetes_min_db_instances
   kubernetes_max_services_instances           = var.kubernetes_max_services_instances
   kubernetes_max_monitoring_instances         = var.kubernetes_max_monitoring_instances
+  kubernetes_min_monitoring_instances         = var.kubernetes_min_monitoring_instances
   kubernetes_max_highmemory_compute_instances = var.kubernetes_max_highmemory_compute_instances
   kubernetes_max_highcpu_compute_instances    = var.kubernetes_max_highcpu_compute_instances
   kubernetes_max_basic_compute_instances      = var.kubernetes_max_basic_compute_instances
@@ -29,6 +30,32 @@ module "create-cluster" {
   kubernetes_nodepool_system_type             = var.kubernetes_nodepool_system_type
   kubernetes_network_plugin                   = var.kubernetes_network_plugin
   temporary_name_for_rotation_system_pool     = var.temporary_name_for_rotation_system_pool
+  kubernetes_min_services_instances           = var.kubernetes_min_services_instances
+  kubernetes_min_highmemory_compute_instances = var.kubernetes_min_highmemory_compute_instances
+  kubernetes_min_basic_compute_instances      = var.kubernetes_min_basic_compute_instances
+  kubernetes_min_system_instances             = var.kubernetes_min_system_instances
+  kubernetes_max_system_instances             = var.kubernetes_max_system_instances
+  kubernetes_max_monitoring_pods              = var.kubernetes_max_monitoring_pods
+  kubernetes_max_db_pods                      = var.kubernetes_max_db_pods
+  kubernetes_max_highmemory_pods              = var.kubernetes_max_highmemory_pods
+  kubernetes_max_services_pods                = var.kubernetes_max_services_pods
+  kubernetes_max_basic_pods                   = var.kubernetes_max_basic_pods
+  kubernetes_max_highcpu_pods                 = var.kubernetes_max_highcpu_pods
+  kubernetes_max_system_pods                  = var.kubernetes_max_system_pods
+  kubernetes_basic_enable_auto_scaling        = var.kubernetes_basic_enable_auto_scaling
+  kubernetes_db_enable_auto_scaling           = var.kubernetes_db_enable_auto_scaling
+  kubernetes_highcpu_enable_auto_scaling      = var.kubernetes_highcpu_enable_auto_scaling
+  kubernetes_highmemory_enable_auto_scaling   = var.kubernetes_highmemory_enable_auto_scaling
+  kubernetes_monitoring_enable_auto_scaling   = var.kubernetes_monitoring_enable_auto_scaling
+  kubernetes_services_enable_auto_scaling     = var.kubernetes_services_enable_auto_scaling
+  kubernetes_system_enable_auto_scaling       = var.kubernetes_system_enable_auto_scaling
+  kubernetes_db_os_disk_size                  = var.kubernetes_db_os_disk_size
+  kubernetes_highcpu_os_disk_size             = var.kubernetes_highcpu_os_disk_size
+  kubernetes_basic_os_disk_size               = var.kubernetes_basic_os_disk_size
+  kubernetes_highmemory_os_disk_size          = var.kubernetes_highmemory_os_disk_size
+  kubernetes_monitoring_os_disk_size          = var.kubernetes_monitoring_os_disk_size
+  kubernetes_services_os_disk_size            = var.kubernetes_services_os_disk_size
+  kubernetes_system_os_disk_size              = var.kubernetes_system_os_disk_size
 
   depends_on = [
     module.create-platform-prerequisite, module.create-network, module.create-privatedns

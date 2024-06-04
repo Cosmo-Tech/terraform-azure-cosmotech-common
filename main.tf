25694312fc6842e69f10d21b3d320ed52da7ec45
nibaldo <nibaldo.donoso@cosmotech.com>
2024-06-04 15:42:16 +0200

    limit length

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  cluster_name    = var.cluster_name != "" ? var.cluster_name : "${var.dns_record}${random_string.cluster_id.result}"
+  cluster_name    = var.cluster_name != "" ? substr(var.cluster_name, 0, 80) : substr("${var.dns_record}${random_string.cluster_id.result}", 0, 80)
   resource_group  = var.resource_group != "" ? substr(var.resource_group, 0, 80) : var.dns_record
   tls_secret_name = var.tls_certificate_type == "let_s_encrypt" ? var.tls_secret_name : "custom-tls-secret"
 }

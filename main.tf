09bface4b1d39c77597c8b1cfb6c5667b5dcd26b
nibaldo <nibaldo.donoso@cosmotech.com>
2024-06-04 14:23:22 +0200

    limit 80 lenght of resouce groupe name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_name    = var.cluster_name != "" ? var.cluster_name : "${var.dns_record}${random_string.cluster_id.result}"
-  resource_group  = var.resource_group != "" ? var.resource_group : var.dns_record
+  resource_group  = var.resource_group != "" ? substr(var.resource_group, 0, 80) : var.dns_record
   tls_secret_name = var.tls_certificate_type == "let_s_encrypt" ? var.tls_secret_name : "custom-tls-secret"
 }
 

c7714a46a6c1d2d9d88b4e3b55bf463c980c05c9
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-07 15:10:13 +0200

    Create argoCD module

--- modules.tf
+++ modules.tf
@@ -145,3 +145,19 @@ module "cosmotech-platform" {
   prom_cpu_mem_request          = var.prom_cpu_mem_request
 
 }
+
+module "create_argocd" {
+  source = "./create_argocd"
+
+  count = var.create_argocd ? 1 : 0
+
+  namespace               = var.argocd_namespace
+  helm_repo_url           = var.argocd_helm_repo_url
+  helm_chart              = var.argocd_helm_chart
+  helm_chart_version      = var.argocd_helm_chart_version
+  helm_release_name       = var.argocd_helm_release_name
+  replicas                = var.argocd_replicas
+  create_ingress          = var.argocd_create_ingress
+  argocd_project          = var.argocd_project
+  argocd_repositories     = var.argocd_repositories
+}

32556ba399740405cd88943c861c85fc6009f5be
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-07 17:33:03 +0200

    Create vault secrets operator module

---
+++ create-vault-secrets-operator/variables.tf
@@ -0,0 +1,19 @@
+variable "namespace" {
+  type = string
+}
+
+variable "helm_repo_url" {
+  type    = string
+}
+
+variable "helm_chart" {
+  type    = string
+}
+
+variable "helm_chart_version" {
+  type    = string
+}
+
+variable "helm_release_name" {
+  type    = string
+}

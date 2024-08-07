e469a3c797426598e86d28964f45574068e984f4
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-07 15:10:13 +0200

    Create argoCD module

---
+++ create_argocd/variables.tf
@@ -0,0 +1,36 @@
+variable "namespace" {
+  type = string
+  default = "argocd"
+}
+
+variable "helm_repo_url" {
+  type    = string
+}
+
+variable "helm_chart" {
+  type    = string
+}
+
+variable "helm_chart_version" {
+  type    = string
+}
+
+variable "helm_release_name" {
+  type    = string
+}
+
+variable "replicas" {
+    type = number
+}
+
+variable "create_ingress" {
+  type = bool
+}
+
+variable "argocd_project" {
+  type = string
+}
+
+variable "argocd_repositories" {
+  type = list(string)
+}
--- modules.tf
+++ modules.tf
@@ -132,3 +132,19 @@ module "cosmotech-platform" {
   is_bare_metal   = var.is_bare_metal
   create_keycloak = var.create_keycloak
 }
+
+module "create_argocd" {
+  source = "./create_argocd"
+
+  count = var.create_argocd ? 1 : 0
+
+  namespace               = var.argocd_namespace
+  helm_repo_url           = var.argocd_helm_repo_url
+  helm_chart              = var.argocd_helm_chart
+  helm_chart_version      = var.argocd_helm_chart_version
+  helm_release_name       = var.argocd_helm_release_name
+  replicas                = var.argocd_replicas
+  create_ingress          = var.argocd_create_ingress
+  argocd_project          = var.argocd_project
+  argocd_repositories     = var.argocd_repositories
+}

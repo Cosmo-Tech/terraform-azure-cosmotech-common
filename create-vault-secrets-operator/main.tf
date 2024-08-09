16886d6c1b0c83f2a242ed57a79998f9ce80875c
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-09 10:08:08 +0200

    Fix RBACs

--- create-vault-secrets-operator/main.tf
+++ create-vault-secrets-operator/main.tf
@@ -49,6 +49,8 @@ resource "helm_release" "vault_secrets_operator" {
   values = [
     templatefile("${path.module}/values.yaml", local.values_vault_secrets_operator)
   ]
+
+  depends_on = [kubernetes_service_account.vault_secrets_operator]
 }
 
 # RBACs
@@ -97,6 +99,13 @@ resource "kubernetes_cluster_role_binding" "vault_secrets_operator" {
   subject {
     kind      = "ServiceAccount"
     name      = kubernetes_service_account.vault_secrets_operator.metadata[0].name
+    namespace = var.namespace
+  }
+}
+
+resource "kubernetes_service_account" "vault_secrets_operator" {
+  metadata {
+    name      = "vault-secrets-operator"
     namespace = var.namespace
   }
 }
@@ -156,27 +165,6 @@ resource "kubernetes_role" "secret_access" {
   }
 }
 
-resource "kubernetes_role" "operator_access" {
-  for_each = toset(var.allowed_namespaces)
-
-  metadata {
-    name      = "vault-secrets-operator-access-${each.key}"
-    namespace = each.key
-  }
-
-  rule {
-    api_groups = [""]
-    resources  = ["secrets"]
-    verbs      = ["get", "list", "watch", "create", "update", "delete"]
-  }
-
-  rule {
-    api_groups = ["secrets.hashicorp.com"]
-    resources  = ["vaultsecrets"]
-    verbs      = ["get", "list", "watch", "create", "update", "delete"]
-  }
-}
-
 resource "kubernetes_role_binding" "secret_access" {
   for_each = toset(var.allowed_namespaces)
 
@@ -196,25 +184,11 @@ resource "kubernetes_role_binding" "secret_access" {
     name      = "default"
     namespace = each.key
   }
-}
 
-resource "kubernetes_role_binding" "operator_access" {
-  for_each = toset(var.allowed_namespaces)
-
-  metadata {
-    name      = "vault-secrets-operator-access-binding-${each.key}"
-    namespace = each.key
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = kubernetes_role.operator_access[each.key].metadata[0].name
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.vault_secrets_operator.metadata[0].name
-    namespace = var.namespace
-  }
+  # If namespace has other serviceaccounts : allow all of them
+  #   subject {
+  #   kind      = "Group"
+  #   name      = "system:serviceaccounts:${each.key}"
+  #   api_group = "rbac.authorization.k8s.io"
+  # }
 }

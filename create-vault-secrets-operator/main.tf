e58d0ff4744010f7b0b78e7b9cb2bf6c89115078
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-21 12:11:15 +0200

    Fix kube version, add providers, fix vars

--- create-vault-secrets-operator/main.tf
+++ create-vault-secrets-operator/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "2.0.4"
-    }
-  }
-}
-
 locals {
   values_vault_secrets_operator = {
     "NAMESPACE"             = var.namespace

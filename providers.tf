b4c6283048f8acaf84275792916ee99de58cb4a5
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-11 14:49:08 +0200

    add option provider_registrations none [skip ci]

--- providers.tf
+++ providers.tf
@@ -27,10 +27,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  resource_provider_registrations = "none"
+  subscription_id                 = var.subscription_id
+  client_id                       = var.client_id
+  client_secret                   = var.client_secret
+  tenant_id                       = var.tenant_id
 }
 
 provider "azuread" {

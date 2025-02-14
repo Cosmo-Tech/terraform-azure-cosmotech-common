45f2723efd962a711bddb8199af643aa6d23ffef
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-14 13:28:16 +0100

    update providers

--- providers.tf
+++ providers.tf
@@ -2,23 +2,31 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.1.0"
+      version = "4.19.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.53.1"
+      version = "3.1.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.32.0"
+      version = "2.35.1"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = "2.0.4"
+      version = "2.1.3"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.15.0"
+      version = "2.17.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
     }
   }
   required_version = ">= 1.3.9"
